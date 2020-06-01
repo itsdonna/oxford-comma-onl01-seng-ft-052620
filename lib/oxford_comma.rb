c879cd62ad191abc759c1bf56d029bae472002b8
Donna Torres <todonnatorres@icloud.com>
2020-06-01 05:43:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 require 'pry'
 def oxford_comma(array)
-  # binding.pry
   if array.length == 1
     array[0]
   elsif array.length == 2
     array[0] + " and " + array[1]
+  else
+  binding.pry
+    
   end
 end
